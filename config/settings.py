18913e9625bc447fd115330c45eb40de2445b304
COREY FSDI-111 <Corey.Jonhson@sdgku.edu>
2024-04-02 23:14:44 -0400

    issue-113 4/2/24

--- config/settings.py
+++ config/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath("templates"))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "accounts.CustomUser"
---
+++ mc2.md
@@ -0,0 +1,8 @@
+# Mini Challenge 2
+
+## Signup, login and logout
+
+### Acceptance Criteria
+1. As an end user, I would like to be able to sign up to the platform via a form.
+2. As an end user, I would like to log into the platform to access features.
+3. As an end user, I would like to log out to terminate my active session on the platform.
